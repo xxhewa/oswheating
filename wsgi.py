08956b7b962625f154f2b53982d2040aff609912
xxhewa <xxhewa@web.de>
2020-02-01 19:25:18 +0100

    update 3

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "OpenShift Hello World 2 !"
+    return "OpenShift Hello World 3!"
 
 if __name__ == "__main__":
     application.run()
