a8cf3291fc8fc59a05c788834cc61a111249da87
xxhewa <xxhewa@web.de>
2020-02-01 18:55:41 +0100

    2 files missing

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "OpenShift Hello World!"
+
+if __name__ == "__main__":
+    application.run()
