96f3653197b44263230ba8259fe6aa436d544735
xxhewa <xxhewa@web.de>
2020-02-02 11:48:43 +0100

    c

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,11 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "OpenShift Hello World 3!"
+    return "OpenShift Hello World 4!"
 
-@application.route("/cars")
-def cars():
-    return '["test1.jpg", "test2.jpg", "test3.jpg"]'
+#@application.route("/cars")
+#def cars():
+#    return '["test1.jpg", "test2.jpg", "test3.jpg"]'
 
 if __name__ == "__main__":
     application.run()
