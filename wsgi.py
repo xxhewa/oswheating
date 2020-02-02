606f69d3c4eb5dd307f603290aae1dda45b6ac36
xxhewa <xxhewa@web.de>
2020-02-02 11:43:42 +0100

    c

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ def hello():
     return "OpenShift Hello World 3!"
 
 @application.route("/cars")
-def hello():
+def cars():
     return '["test1.jpg", "test2.jpg", "test3.jpg"]'
 
 if __name__ == "__main__":
