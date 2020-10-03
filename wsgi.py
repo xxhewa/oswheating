2620181a05a6251bd54d383246b416e0ef2cf580
hwagner1 <xxhewa@web.de>
2020-10-03 12:31:42 +0200

    removed db2

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,9 @@
 import os
 import json
 from flask import Flask
-from db2client import db2Access
 from flask import make_response
 from flask import request
 from requests import get
-
-mydb2 = db2Access()
 
 application = Flask(__name__)
 
@@ -16,13 +13,11 @@ application = Flask(__name__)
 
 @application.route("/cars")
 def cars():
-    r = make_response(mydb2.get_categorized_images('C'))
     r.headers.set('Access-Control-Allow-Origin', 'appdomain.cloud')
     return r
 
 @application.route("/persons")
 def persons():
-    r = make_response(mydb2.get_categorized_images('P'))
     r.headers.set('Access-Control-Allow-Origin', 'appdomain.cloud')
     return r
 
