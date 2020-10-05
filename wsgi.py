f83435ab207892cf2af4de08d055d1f18ad16892
hwagner1 <xxhewa@web.de>
2020-10-05 11:36:47 +0200

    fixed welcome message

--- wsgi.py
+++ wsgi.py
@@ -15,11 +15,11 @@ temperature4 = 0
 @application.route("/")
 def hello():
 
-    result = "Hello World on Openshift, here is another update!<br>"
-    result = result + "Temperature 1:"+str(temperature1)+ " °C<br>"
-    result = result + "Temperature 2:"+str(temperature2)+ " °C<br>"
-    result = result + "Temperature 3:"+str(temperature3)+ " °C<br>"
-    result = result + "Temperature 4:"+str(temperature4)+ " °C<br>" 
+    result = "Hello World on Openshift, here are the temperatures:<br>"
+    result = result + "Temperature 1: "+str(temperature1)+ " °C<br>"
+    result = result + "Temperature 2: "+str(temperature2)+ " °C<br>"
+    result = result + "Temperature 3: "+str(temperature3)+ " °C<br>"
+    result = result + "Temperature 4: "+str(temperature4)+ " °C<br>" 
     return result
 
 @application.route("/metrics")
